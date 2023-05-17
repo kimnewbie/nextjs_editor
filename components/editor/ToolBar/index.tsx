7a299820992e8b39736e6358272349fa32e686e7
youjin kim <gracecancho@gmail.com>
2023-05-17 10:12:36 +0900

    link 제외하고 기능 넣기

--- components/editor/ToolBar/index.tsx
+++ components/editor/ToolBar/index.tsx
@@ -45,37 +45,37 @@ const ToolBar: FC<Props> = ({ editor }): JSX.Element | null => {
             />
             <div className="h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8" />
             <div className="flex items-center space-x-3">
-                <Button>
+                <Button active={editor.isActive('bold')} onClick={() => getFocusedEditor(editor).toggleBold().run()}>
                     <BsTypeBold />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('italic')} onClick={() => getFocusedEditor(editor).toggleItalic().run()}>
                     <BsTypeItalic />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('underline')} onClick={() => getFocusedEditor(editor).toggleUnderline().run()}                >
                     <BsTypeUnderline />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('strike')} onClick={() => getFocusedEditor(editor).toggleStrike().run()}>
                     <BsTypeStrikethrough />
                 </Button>
             </div>
             <div className="h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8" />
             <div className="flex items-center space-x-3">
-                <Button>
+                <Button active={editor.isActive('blockquote')} onClick={() => getFocusedEditor(editor).toggleBlockquote().run()}>
                     <RiDoubleQuotesL />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('code')} onClick={() => getFocusedEditor(editor).toggleCode().run()}>
                     <BsCode />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('codeBlock')} onClick={() => getFocusedEditor(editor).toggleCodeBlock().run()}>
                     <BsBraces />
                 </Button>
-                <Button>
+                <Button onClick={() => { }}>
                     <BsLink45Deg />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('orderedList')} onClick={() => getFocusedEditor(editor).toggleOrderedList().run()}>
                     <BsListOl />
                 </Button>
-                <Button>
+                <Button active={editor.isActive('bulletList')} onClick={() => getFocusedEditor(editor).toggleBulletList().run()}>
                     <BsListUl />
                 </Button>
             </div>
--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,12 +1,27 @@
 import { FC } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import Placeholder from '@tiptap/extension-placeholder';
 import ToolBar from './ToolBar';
 
 interface Props { }
 
 const Editor: FC<Props> = (props): JSX.Element => {
-    const editor = useEditor({ extensions: [StarterKit] });
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Underline,
+            Placeholder.configure({
+                placeholder: "Type something"
+            })
+        ],
+        editorProps: {
+            attributes: {
+                class: 'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full'
+            }
+        }
+    });
     return (
         <div className='p-3 dark:bg-primary-dark bg-primary transition'>
             {/* <button onClick={() => {
@@ -14,6 +29,7 @@ const Editor: FC<Props> = (props): JSX.Element => {
                 editor.chain().focus().toggleBold().run();
             }}>Bold</button> */}
             <ToolBar editor={editor} />
+            <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3"></div>
             <EditorContent editor={editor} />
         </div>
     )
